4fd44da4b99f554efa537b74bf9c8c8bafdbd55a
Aidan <quantumaidan@gmail.com>
2024-01-26 00:32:10 -0600

    first

---
+++ src/main/java/net/quantumaidan/itemlore/ItemLore.java
@@ -0,0 +1,16 @@
+package net.quantumaidan.itemlore;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ItemLore implements ModInitializer {
+	public static final String MOD_ID = "itemlore";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/quantumaidan/itemlore/ItemLoreClient.java
@@ -0,0 +1,10 @@
+package net.quantumaidan.itemlore;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ItemLoreClient implements ClientModInitializer {
+    @Override
+    public  void onInitializeClient() {
+
+    }
+}
