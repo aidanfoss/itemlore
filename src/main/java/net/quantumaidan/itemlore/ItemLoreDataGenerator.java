4923e41ec6a054f9769dcb7819c5f33ebcacc7eb
Aidan <quantumaidan@gmail.com>
2024-02-02 01:57:51 -0600

    readded dataGenerator. Not sure why its crashing now, i deleted it just fine earlier.
    Also adding icon for mod viewer

---
+++ src/main/java/net/quantumaidan/itemlore/ItemLoreDataGenerator.java
@@ -0,0 +1,11 @@
+package net.quantumaidan.itemlore;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ItemLoreDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		//likely not needed
+	}
+}
