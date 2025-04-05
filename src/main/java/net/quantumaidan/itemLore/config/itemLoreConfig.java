f964bce07b183d741ea686f76b8acf9eb79c02b8
Aidan Foss <quantumaidan@gmail.com>
2025-04-05 13:39:04 -0500

    added comments for the config

--- src/main/java/net/quantumaidan/itemLore/config/itemLoreConfig.java
+++ src/main/java/net/quantumaidan/itemLore/config/itemLoreConfig.java
@@ -6,8 +6,8 @@ public class itemLoreConfig extends MidnightConfig {
     @Entry(name = "Toggle")
     public static boolean enabled = true;
 
-    @Comment
-    public static String comment1 = "DO NOT TOUCH BELOW IF YOU DON'T KNOW WHAT YOU ARE DOING";
+    @Comment(name = "Defaults if input is nonsense", centered = true) public static String comment1;
+    @Comment(name = "Formatting Information on Modrinth", centered = true) public static String comment2;
 
     @Entry(name = "DateTimeFormat")
     public static String dateTimeFormatConfig = "MM/dd/yyyy hh:mm a";
