636eb8f3f2eeabcaf2c11786b41aabc69c2407d5
Aidan Foss <quantumaidan@gmail.com>
2025-12-24 23:44:55 -0600

    adding sgtveinminer compat

---
+++ src/main/java/net/quantumaidan/itemLore/mixin/CompatMixinPlugin.java
@@ -0,0 +1,50 @@
+package net.quantumaidan.itemLore.mixin;
+
+import net.fabricmc.loader.api.FabricLoader;
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+
+import java.util.List;
+import java.util.Set;
+
+public class CompatMixinPlugin implements IMixinConfigPlugin {
+
+    @Override
+    public void onLoad(String mixinPackage) {
+        // No-op
+    }
+
+    @Override
+    public String getRefMapperConfig() {
+        return null;
+    }
+
+    @Override
+    public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+        if (mixinClassName.contains("sgtVeinMinerMixin")) {
+            return FabricLoader.getInstance().isModLoaded("veinminermod");
+        }
+        return true;
+    }
+
+    @Override
+    public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
+        // No-op
+    }
+
+    @Override
+    public List<String> getMixins() {
+        return null;
+    }
+
+    @Override
+    public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+        // No-op
+    }
+
+    @Override
+    public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+        // No-op
+    }
+}
