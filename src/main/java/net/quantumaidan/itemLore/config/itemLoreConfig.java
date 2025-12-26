c47cef98e85f442623f77b0765da25c081fb895c
Aidan Foss <quantumaidan@gmail.com>
2025-12-25 20:00:01 -0600

    fixing modmenu integration

--- src/main/java/net/quantumaidan/itemLore/config/itemLoreConfig.java
+++ src/main/java/net/quantumaidan/itemLore/config/itemLoreConfig.java
@@ -3,30 +3,33 @@ package net.quantumaidan.itemLore.config;
 import eu.midnightdust.lib.config.MidnightConfig;
 
 public class itemLoreConfig extends MidnightConfig {
-    @Entry(name = "Toggle")
+    @Entry(name = "Toggle", category = "server")
     public static boolean enabled = true;
 
-    @Entry(name = "Force Lore Mode")
+    @Entry(name = "Force Lore Mode", category = "server")
     public static ForceLoreMode forceLoreMode = ForceLoreMode.UNSTACKABLE;
 
     public enum ForceLoreMode {
         ALL, UNSTACKABLE, OFF
     }
 
-    @Comment(name = "Defaults if input is nonsense", centered = true)
-    public static String comment1;
-    @Comment(name = "Formatting Information on Modrinth", centered = true)
-    public static String comment2;
-
-    @Entry(name = "Date Time")
+    @Entry(name = "Date Time", category = "server")
     public static String dateTimeFormatConfig = "MM/dd/yyyy hh:mm a";
 
-    @Entry(name = "Time Zone")
+    @Entry(name = "Time Zone", category = "server")
     public static String timeZone = "CST";
 
-    @Entry(name = "Relevant Blocks")
+    @Comment(name = "Defaults if input is nonsense", centered = true, category = "server")
+    public static String comment1;
+    @Comment(name = "Formatting Information on Modrinth", centered = true, category = "server")
+    public static String comment2;
+
+    @Entry(name = "Relevant Blocks", category = "server")
     public static String relevantBlocks = "Copper,Gold,Iron,Coal,Lapis,Redstone,Emerald,Diamond,Quartz,Stone,Deepslate";
 
-    @Entry(name = "Relevant Mobs")
+    @Entry(name = "Relevant Mobs", category = "server")
     public static String relevantMobs = "Zombie,Creeper,Skeleton,Spider,Enderman";
+
+    @Comment(name = "There is no settings on the client-side just yet", centered = true, category = "client")
+    public static String comment3;
 }
