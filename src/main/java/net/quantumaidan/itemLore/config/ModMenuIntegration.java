d620934aa417bf314db2280c4f7620681cf09461
Aidan Foss <quantumaidan@gmail.com>
2025-12-29 14:49:07 -0600

    removing attempt at TreeHarvesterMixin, moving it to a branch

--- src/main/java/net/quantumaidan/itemLore/config/ModMenuIntegration.java
+++ src/main/java/net/quantumaidan/itemLore/config/ModMenuIntegration.java
@@ -1,4 +1,4 @@
-package net.quantumaidan.itemLore.compat;
+package net.quantumaidan.itemLore.config;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
--- src/main/java/net/quantumaidan/itemLore/mixin/CompatMixinPlugin.java
+++ src/main/java/net/quantumaidan/itemLore/mixin/CompatMixinPlugin.java
@@ -25,9 +25,9 @@ public class CompatMixinPlugin implements IMixinConfigPlugin {
         if (mixinClassName.contains("sgtVeinMinerMixin")) {
             return FabricLoader.getInstance().isModLoaded("veinminermod");
         }
-        if (mixinClassName.contains("TreeHarvesterMixin")) {
-            return FabricLoader.getInstance().isModLoaded("treeharvester");
-        }
+//        if (mixinClassName.contains("TreeHarvesterMixin")) {
+//            return FabricLoader.getInstance().isModLoaded("treeharvester");
+//        }
         return true;
     }
 
